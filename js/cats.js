ddf98aa83b9acdc60e7ceb929f9964172b38c7e8
Sonja Krause-Harder <skh@rcane.de>
2015-09-17 21:06:53 +0200

    Add more cats

--- js/cats.js
+++ js/cats.js
@@ -8,5 +8,20 @@ var cats = [
 		"name": "Murr",
 		"imgsrc": "img/murr.jpg",
 		"clicks": 0
+	},
+	{
+		"name": "Hunter",
+		"imgsrc": "img/hunter.jpg",
+		"clicks": 0
+	},
+	{
+		"name": "Clara",
+		"imgsrc": "img/clara.jpg",
+		"clicks": 0
+	},
+	{
+		"name": "Y T",
+		"imgsrc": "img/yt.jpg",
+		"clicks": 0
 	}
 ];
