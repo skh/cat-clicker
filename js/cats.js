a8843daa2c0c012c0fb01758cefa3599632cf142
Sonja Krause-Harder <skh@rcane.de>
2015-09-20 21:15:54 +0200

    Add more cats again

---
+++ js/cats.js
@@ -0,0 +1,33 @@
+var cats = [
+	{
+		"name": "Catherine",
+		"imgsrc": "img/cat.jpg",
+		"clicks": 0,
+		"nicknames": ["Catty", "Tabby", "Devil"]
+	},
+	{
+		"name": "Murr",
+		"imgsrc": "img/murr.jpg",
+		"clicks": 0,
+		"nicknames": ["Kater", "Nicky", "Daemon"]
+	},
+	{
+		"name": "Hunter",
+		"imgsrc": "img/hunter.jpg",
+		"clicks": 0,
+		"nicknames": ["Fearless Fighter", "Ariadne"]
+	},
+	{
+		"name": "Clara",
+		"imgsrc": "img/clara.jpg",
+		"clicks": 0,
+		"nicknames": ["Lady", "Beauty", "HUngry"]
+	},
+	{
+		"name": "Y T",
+	
+		"imgsrc": "img/yt.jpg",
+		"clicks": 0,
+		"nicknames": ["Whitey", "Sleepy"]
+	}
+];
--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
+var Cat = function (data) {
+  this.clickCount = ko.observable(data.clicks);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgsrc);
+  this.nicknames = ko.observableArray(data.nicknames);
+  this.level = ko.computed(function () {
+    var count = this.clickCount();
+    if (count < 10) {
+      return 'Newborn';
+    } else if (count < 50) {
+      return 'Infant';
+    } else if (count < 100) {
+      return 'Teen';
+    } else {
+      return 'Adult';
+    }
+  }, this);
+};
+
 var ViewModel = function () {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Catherine');
-  this.imgSrc = ko.observable('img/cat.jpg');
+  
+  var self = this;
+  this.catList = ko.observableArray([]);
+  // cats in cats.js
+  cats.forEach(function (catItem) {
+    self.catList().push(ko.observable(new Cat(catItem)));
+  });
+
+  this.currentCat = this.catList()[0];
+
+  this.incrementCounter = function () {
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  };
+
+  this.selectCat = function (cat) {
+    self.currentCat(cat);
+  }
 };
 
 ko.applyBindings (new ViewModel ());
